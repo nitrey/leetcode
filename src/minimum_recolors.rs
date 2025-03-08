8e4f0f0a236699d1d60ed113ba68e2d37262769d
Alexander Antonov <nsnitrey@gmail.com>
2025-03-08 14:56:00 +0100

    Format code

--- src/minimum_recolors.rs
+++ src/minimum_recolors.rs
@@ -2,18 +2,15 @@ use std::cmp::min;
 
 pub fn minimum_recolors(blocks: String, k: i32) -> i32 {
     let window_size = k as usize;
-    let is_black: Vec<bool> = blocks
-      .chars()
-      .map(|c| c == 'B')
-      .collect();
+    let is_black: Vec<bool> = blocks.chars().map(|c| c == 'B').collect();
     let mut black_count = is_black[0..window_size].iter().filter(|&&x| x).count();
     let mut min_recolors = window_size - black_count;
     for i in window_size..is_black.len() {
         if is_black[i - window_size] {
-          black_count -= 1;
+            black_count -= 1;
         }
         if is_black[i] {
-          black_count += 1;
+            black_count += 1;
         }
         let recolors = window_size - black_count;
         min_recolors = min(min_recolors, recolors);
