edb6bc22f4d1fd215a99747692f7ff8ece3b27bf
Alexander Antonov <nsnitrey@gmail.com>
2025-03-12 13:51:31 +0100

    Implement inition IPO solution

--- src/ipo.rs
+++ src/ipo.rs
@@ -1,1 +1,33 @@
 // https://leetcode.com/problems/ipo/
+
+use std::collections::BinaryHeap;
+
+struct Project {
+    capital: i32,
+    profit: i32,
+}
+
+pub fn find_maximized_capital(k: i32, w: i32, profits: Vec<i32>, capital: Vec<i32>) -> i32 {
+  let mut projects: Vec<Project> = capital
+      .into_iter()
+      .zip(profits.into_iter())
+      .map(|(capital, profit)| Project { capital, profit })
+      .collect();
+  projects.sort_unstable_by_key(|elem| elem.capital);
+
+  let mut total_profit = w;
+  let mut next_index = 0;
+  let mut opportunities = BinaryHeap::with_capacity(projects.len());
+
+  for _ in 0..k {
+      while let Some(proj) = projects.get(next_index).filter(|proj| proj.capital <= total_profit) {
+          opportunities.push(proj.profit);
+          next_index += 1;
+      }
+      if let Some(max_profit) = opportunities.pop() {
+          total_profit += max_profit;
+      }
+  }
+
+  total_profit
+}
